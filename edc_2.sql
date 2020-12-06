af10be9da0567f44279dc32d011dd8930f202b69
bbsebb <sebastien.burckhardt@gmail.com>
2020-12-06 03:18:44 +0100

    tt

--- edc_2.sql
+++ edc_2.sql
@@ -103,7 +103,7 @@ WHERE LOWER(adrpers) LIKE '%reims%';
 SELECT titrefilm, annee, duree
 FROM film
 WHERE codegenre = 
-    (SELECT DISTINCT codegenre 
+    (SELECT codegenre 
     FROM genre 
     WHERE LOWER(intgenre) LIKE '%science-fiction%')
 -- 2.6
@@ -162,4 +162,151 @@ WHERE et.codefilm IN (
     LOWER(titrefilm) LIKE '%ast_rix et ob_lix contre c_sar%' OR
     LOWER(titrefilm) LIKE '%the faculty%');
 -- 2.12
-SELECT 
+SELECT titrefilm
+FROM film
+WHERE codefilm IN (
+    SELECT DISTINCT codefilm 
+    FROM emprunt
+    WHERE dateretour IS NULL
+    EXCEPT
+    SELECT DISTINCT codefilm 
+    FROM emprunt
+    WHERE dateretour IS NOT NULL);
+-- 2.13
+SELECT titrefilm 
+FROM film
+WHERE codegenre IN (
+    SELECT codegenre 
+    FROM genre 
+    WHERE LOWER(intgenre) LIKE '%divers%')
+    AND codefilm IN ( 
+    SELECT codefilm 
+    FROM acteur 
+    JOIN jouer_un_role USING(codeacteur)
+    WHERE LOWER(prenomacteur) LIKE '%g_rard%' AND LOWER(nomacteur) LIKE '%depardieu%');
+-- 2.14
+SELECT titrefilm,annee,duree
+FROM film 
+WHERE annee = '1986' AND (duree = 110 OR duree = 120);
+-- 2.15
+SELECT count(*) 
+FROM film
+WHERE codefilm IN (SELECT e.codefilm 
+    FROM exemplaire e 
+    JOIN support s USING(codesupport)
+    WHERE s.intsupport = 'DVD')
+-- 2.16
+BEGIN TRANSACTION;
+CREATE TABLE serie (
+    codeserie int,
+    intserie varchar,
+    CONSTRAINT PK_serie PRIMARY KEY (codeserie)
+);
+ALTER TABLE film ADD COLUMN codeserie int REFERENCES serie(codeserie);
+-- 2.17
+INSERT INTO serie VALUES 
+    (1,'TRILOGIE RAMBO'),
+    (2,'TRILOGIE BEST OF THE BEST'),
+    (3,'JAWS'),
+    (4,'TRILOGIE SCREAM') ;
+-- 2.18 
+UPDATE film SET codeserie = 1 WHERE LOWER(titrefilm) = 'rambo';
+UPDATE film SET codeserie = 2 WHERE LOWER(titrefilm) = 'best of the best 2';
+UPDATE film SET codeserie = 1 WHERE LOWER(titrefilm) = 'dents de la mer (les)';
+UPDATE film SET codeserie = 1 WHERE LOWER(titrefilm) = 'scream';
+COMMIT;
+-- 2.19
+SELECT f.titrefilm, a.nomacteur || ' ' || a.prenomacteur 
+FROM film f
+JOIN jouer_un_role USING(codefilm)
+JOIN acteur a USING(codeacteur);
+-- 2.20
+SELECT titrefilm, intgenre
+FROM film
+JOIN genre USING(codegenre)
+ORDER BY 2;
+-- 2.21
+SELECT f.titrefilm, e.datepret,e.dateretour
+FROM film f
+JOIN exemplaire USING(codefilm)
+JOIN emprunt e USING(codefilm,numexemplaire)
+WHERE e.codepers = (
+    SELECT codepers
+    FROM emprunteur
+    WHERE LOWER(nompers) = 'taparov' AND LOWER(prenompers) = 'igor');
+-- 2.22 
+SELECT DISTINCT f.titrefilm, g.intgenre, a.nomacteur || ' ' || a.prenomacteur, s.intsupport 
+FROM film f
+JOIN genre g USING(codegenre) 
+JOIN jouer_un_role USING(codefilm)
+JOIN acteur a USING(codeacteur)
+JOIN exemplaire USING(codefilm)
+JOIN support s USING(codesupport)
+WHERE UPPER(f.titrefilm) LIKE 'S%';
+-- 2.23
+SELECT f.titrefilm, count(e.codesupport) AS "nombre exemplaires"
+FROM film f
+JOIN exemplaire e USING(codefilm)
+GROUP BY f.titrefilm
+HAVING count(e.codesupport)>1;
+-- 2.24
+SELECT g.intgenre, SUM(f.duree) AS "somme durées en min"
+FROM genre g
+JOIN film f USING (codegenre)
+GROUP BY g.intgenre;
+-- 2.25
+SELECT g.intgenre, count(f.*) AS "nombre de film"
+FROM genre g
+JOIN film f USING (codegenre)
+GROUP BY g.intgenre
+HAVING count(f.*)>4;
+-- 2.26
+SELECT titrefilm
+FROM film
+WHERE duree>(SELECT AVG(duree) FROM film);
+-- 2.27
+SELECT AVG(duree) FROM film
+-- 2.28
+SELECT UPPER(SUBSTR(titrefilm,0,1)) || SUBSTR(titrefilm,1)
+FROM film;
+-- 2.29
+SELECT titrefilm, 'Longueur : ' || LENGTH(titrefilm) || ' caractères' AS "nbr caractère"
+FROM film
+WHERE LENGTH(titrefilm) = (
+    SELECT MAX(LENGTH(titrefilm))
+    FROM film
+)
+-- 2.30
+SELECT f.titrefilm || ' est sorti de la videothèque depuis ' ||  ( CURRENT_DATE - em.datepret ) ||' jours'
+FROM film f
+JOIN exemplaire ex USING(codefilm)
+JOIN emprunt em USING(codefilm,numexemplaire)
+WHERE em.dateretour>CURRENT_DATE AND NOT EXISTS (
+    SELECT * 
+    FROM emprunt em2
+    WHERE em2.codefilm = f.codefilm AND (em.dateretour<=CURRENT_DATE OR em.dateretour IS NULL)
+)
+-- 2.31
+SELECT titrefilm || ' (VF)'
+FROM film;
+SELECT CONCAT(titrefilm,' ','(VF)')
+FROM film;
+-- 2.32
+SELECT CONCAT('00 33 ',REPLACE(SUBSTR(telpers,1),'.',' '))
+FROM emprunteur;
+-- 2.33
+SELECT DISTINCT titrefilm
+FROM film
+WHERE codefilm IN (
+    SELECT codefilm
+    FROM emprunt em
+    WHERE EXTRACT('year' FROM em.datepret ) = 2001 AND EXTRACT('month' FROM em.datepret ) = 1)
+-- 2.34
+SELECT titrefilm, 
+    CASE WHEN duree IS NULL THEN 'Durée non renseigné'  
+        ELSE CAST(duree AS varchar)
+    END AS "durée",
+    CASE WHEN annee IS NULL THEN 'Année non renseigné'
+        ELSE annee
+    END AS "année"
+FROM film;
