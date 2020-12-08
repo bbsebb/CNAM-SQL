09abfb72a836c330dd7bfb39e99fb92941a6db41
bbsebb <sebastien.burckhardt@gmail.com>
2020-12-08 03:13:02 +0100

    plsql 1

--- NFA011/plsql1.sql
+++ NFA011/plsql1.sql
@@ -42,9 +42,42 @@ RETURN rtr;
 END
 '
 CREATE OR REPLACE FUNCTION fibRec(nbr integer) RETURNS integer LANGUAGE plpgsql AS '
-DECLARE
-rtr integer;
 BEGIN
-IF 
-
+IF nbr = 0 
+    THEN RETURN 0;
+ELSIF nbr = 1
+    THEN RETURN 1;
+ELSE 
+    RETURN fibRec(nbr-1)+fibRec(nbr-2);
+END IF;
+END
 '
+/* Écrire la déclaration de la fonction Harmonique qui calcule le ne terme de la série de harmonique.
+ */
+ CREATE OR REPLACE FUNCTION Harmonique(nbr integer) RETURNS numeric LANGUAGE plpgsql AS '
+ DECLARE
+ a numeric;
+ BEGIN
+ a:=0;
+ FOR compteur IN 1..nbr LOOP
+    a = a + (1/CAST(compteur AS numeric));
+END LOOP;
+RETURN a;
+END
+ '
+
+CREATE OR REPLACE FUNCTION HarmoniqueRec(nbr numeric) RETURNS numeric LANGUAGE plpgsql AS '
+DECLARE
+rtr numeric;
+BEGIN
+rtr :=0;
+IF nbr = 1 
+    THEN rtr := 1+rtr;
+ELSE 
+    rtr := HarmoniqueRec(nbr-1) + 1/nbr;
+END IF;
+RETURN rtr;
+END
+'
+
+
