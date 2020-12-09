6481ddb030efc794c8604f3376bd5abc9793e82c
bbsebb <sebastien.burckhardt@gmail.com>
2020-12-09 03:49:24 +0100

    plsql2 fin

--- NFA011/plsql2.sql
+++ NFA011/plsql2.sql
@@ -1,9 +1,25 @@
 CREATE OR REPLACE FUNCTION supcompte(num_compte integer) RETURNS void LANGUAGE plpgsql AS '
-
 DECLARE
-
-tuple_client client%ROWTYPE;
+tuple_compte compte%ROWTYPE;
+nbr_compte INTEGER;
+num_client client.code_client%TYPE;
 BEGIN
+    SELECT DISTINCT code_client INTO num_client FROM compte WHERE no_compte = num_compte;
+    SELECT count(*) INTO nbr_compte FROM compte WHERE code_client = num_client;
+    SELECT * INTO tuple_compte FROM compte WHERE no_compte = num_compte;
 
+    INSERT INTO ancclient SELECT * FROM client WHERE code_client = num_client ON CONFLICT DO NOTHING;
+    INSERT INTO anccompte 
+    VALUES (num_compte,tuple_compte.date_creation,CURRENT_DATE,tuple_compte.code_client);
+
+    DELETE FROM ecriture WHERE no_compte = num_compte;
+    DELETE FROM associer WHERE no_compte = num_compte;
+    DELETE FROM compte WHERE no_compte = num_compte;
+
+    IF nbr_compte = 1 THEN 
+        DELETE FROM client WHERE code_client = num_client;
+    END IF;
 END
-'
+';
+
+
